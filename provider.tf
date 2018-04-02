1bba8e2618920b027e7a92930555bd9e9c335f0a
GopiK <gopiawslab@gmail.com>
2018-04-02 13:58:01 -0700

    new tf file created

---
+++ provider.tf
@@ -0,0 +1,8 @@
+# Configure the Azure Provider
+provider "azurerm" { }
+
+# Create a resource group
+resource "azurerm_resource_group" "network" {
+  name     = "production"
+  location = "West US"
+}
